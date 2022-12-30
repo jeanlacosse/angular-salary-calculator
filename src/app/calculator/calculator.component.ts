f5abe8cd6b93b2ae4375acbebf02ec6aa9abfcbc
Jean-Luc LaCosse <jeanlacosse@gmail.com>
2022-12-30 09:56:10 -0600

    created app for calculations

--- src/app/calculator/calculator.component.ts
+++ src/app/calculator/calculator.component.ts
@@ -1,9 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-calc',
-  templateUrl: './calculator.component.html',
-  styleUrls: ['./calculator.component.html']
+    selector: 'app-calc',
+    templateUrl: './calculator.component.html',
+    styleUrls: ['./calculator.component.css']
 })
 export class CalculatorComponent {
     firstName: string = "";
@@ -11,23 +11,38 @@ export class CalculatorComponent {
     idNumber: number = 0;
     jobTitle: string = "";
     annualSalary: number = 0;
+    totalMonthlyCost: number = 0;
 
     allEmployees: any[] = []
 
     addNewEmp() {
 
+        
         let newEmployee = {
             firstName: this.firstName,
             lastName: this.lastName,
             idNumber: this.idNumber,
             jobTitle: this.jobTitle,
             annualSalary: this.annualSalary
-        }
+        };
 
-        this.allEmployees.push(newEmployee)
+        this.allEmployees.push(newEmployee);
+
+        let totalDisplay = this.totalMonthlyCost += (newEmployee.annualSalary / 12);
+        totalDisplay.toFixed(2);
+        this.totalMonthlyCost = totalDisplay;
+
+        this.firstName = "";
+        this.lastName = "";
+        this.idNumber = 0;
+        this.jobTitle = "";
+        this.annualSalary = 0;
+
+        // if (this.totalMonthlyCost > 20000) {
+        //     this.ngClass = {'overBudget': true}
+        // }
+
         console.log("all employees", this.allEmployees);
-        console.log("second employee lastname", this.allEmployees[1].lastName);
 
-        // every time clicked add info to an array and the do an ngfor loop of that array in the html
     }
 }
